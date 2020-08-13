0510d597c05eecb17b1ca3c55a1501f6989a925e
jiwoon20 <mjlk2001@gmail.com>
2020-08-13 22:01:16 +0900

    day2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Button, Progress} from 'semantic-ui-react'
+import {Button, Input} from 'semantic-ui-react'
+
+function Login2(){
+
+  return(
+    <div>
+    <Input/>
+    <Button onClick = { (e) => {
+      console.log(e)
+      alert(e.target.value)
+    } } > 로그인 </Button>
+    </div>
+
+  )
+}
+
+class Login extends React.Component{
+  constructor(){
+    super()
+    this.state = {id : "아무것도 입력하지 않았습니다"}
+  }
+
+  render(){
+    return(
+      <div>
+      <Input onchange = { (e) => this.setState({id : e.target.value })  }/>
+      <Button value = {this.state.id} onClick = { (event) => {
+        alert(event.target.value)
+      } } > Login </Button>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div>
-    <p> 안녕하 세요 ! </p>
-    <Button Primary> hi </Button>
-    <Progress percent = {33} indicating/>
+    <p> 안녕하세요 </p>
+    <Button> hi </Button>
+    <Login / >
     </div>
   );
 }
