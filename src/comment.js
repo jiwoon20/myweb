c9a870870b07c9a0d19bfe07ac236cd8ad9a74a9
jiwoon20 <mjlk2001@gmail.com>
2020-08-20 23:48:48 +0900

    day3

--- src/comment.js
+++ src/comment.js
@@ -1,41 +1,64 @@
-import React from 'react';
-import {Comment, Form, Button, Header, Icon} from 'semantic-ui-react'
+import React from "react";
+import { Comment, Form, Button, Header, Icon } from "semantic-ui-react";
 
+import human from "./human.png";
 
-import human from "./human.png"
+function SingleComment(detail) {
+  return (
+    <Comment>
+      <Comment.Avatar src={human} />
+      <Comment.Content>
+        <Comment.Author as="a">Matt</Comment.Author>
+        <Comment.Metadata>
+          <div>Today at 5:42PM</div>
+        </Comment.Metadata>
+        <Comment.Text>{detail.content}</Comment.Text>
+      </Comment.Content>
+    </Comment>
+  );
+}
 
-class Comments extends React.Component{
+class Comments extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputContent: "",
+      commentslist: [],
+    };
+  }
 
-  render(){
-    return(    <Comment.Group style = {{marginLeft :"550px"} }>
-      <Header as='h3' dividing>
-        Comments
-      </Header>
+  render() {
+    return (
+      <Comment.Group style={{ marginLeft: "550px" }}>
+        <Header as="h3" dividing>
+          Comments
+        </Header>
 
-      <Comment>
-        <Comment.Avatar src={human} />
-        <Comment.Content>
-          <Comment.Author as='a'>Matt</Comment.Author>
-          <Comment.Metadata>
-            <div>Today at 5:42PM</div>
-          </Comment.Metadata>
-          <Comment.Text>How artistic!</Comment.Text>
-          <Comment.Actions>
-            <Comment.Action>Reply</Comment.Action>
-          </Comment.Actions>
-        </Comment.Content>
-      </Comment>
+        {this.state.commentslist.map(comments => <SingleComment content = {comments}/> )}
 
-
-
-      <Form reply>
-        <Form.TextArea />
-        <Button content='Add Reply' labelPosition='left' icon='edit' primary />
-      </Form>
-
-    
-    </Comment.Group>)
+        <Form reply>
+          <Form.TextArea
+            value={this.state.inputContent}
+            placeholder="댓글을 입력해주세요"
+            onChange={(e) => this.setState({ inputContent: e.target.value })}
+          />
+          <Button
+            content="Add Reply"
+            labelPosition="left"
+            icon="edit"
+            primary
+            onClick={() =>
+              this.setState((prevState) => {
+                return {
+                  commentslist: [...prevState.commentslist, this.state.inputContent],
+                };
+              })
+            }
+          />
+        </Form>
+      </Comment.Group>
+    );
   }
 }
 
-export default Comments
+export default Comments;
